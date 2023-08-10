a34148a3f244fb7f90245feb769465936992e7b8
reagan-meant <reaganmeant@gmail.com>
2023-08-10 12:42:07 +0300

    Add ansible installation script

---
+++ install_ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# Upgrade installed packages
+sudo apt upgrade -y
+
+# Install Ansible
+sudo apt install ansible -y
+
+echo "Ansible installed successfully."
